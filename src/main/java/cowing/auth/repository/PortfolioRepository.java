1f303691b15aa225400df4754a964bb81f461a1b
yuna <nissy930@naver.com>
2025-07-19 13:57:56 +0900

    Feat(#20) : 파산 후 포트폴리오 조회 메서드 추가
    -파산 후에는 파산 시간 이후에 기록된 포트폴리오만 조회 가능

--- src/main/java/cowing/auth/repository/PortfolioRepository.java
+++ src/main/java/cowing/auth/repository/PortfolioRepository.java
@@ -4,9 +4,11 @@ import cowing.auth.dto.PortfolioDto;
 import cowing.auth.entity.Portfolio;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
     List<Portfolio> findByUsername(String username);
+    List<Portfolio> findByUsernameAndCreatedAtAfter(String username, LocalDateTime bankruptAt);
 
 }
